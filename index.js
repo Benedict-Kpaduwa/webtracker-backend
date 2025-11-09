4c1dd379f9dc95631bc3ff5e8ff40ab3c94e3dc6
Benedict Kpaduwa <benedictkpaduwa@gmail.com>
2025-11-08 19:31:31 -0700

    index js fixes

--- index.js
+++ index.js
@@ -10,24 +10,13 @@ import visitorRoutes from "./routes/visitor.js";
 import statsRoutes from "./routes/stats.js";
 import adminRoutes from "./routes/admin.js";
 
-// const allowedOrigins = [
-//     "http://localhost:4000",
-//     "https://isec-603-web-tracker.vercel.app"
-// ];
-
 dotenv.config();
-
 const app = express();
+
 app.use(helmet());
 app.use(cors({ origin: "*" }));
-
 app.use(express.json({ limit: "200kb" }));
-app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => { console.error("MongoDB connection error:", err); process.exit(1); });
 
 app.use("/track", trackRoutes);
 app.use("/visitors", visitorRoutes);
@@ -35,5 +24,12 @@ app.use("/stats", statsRoutes);
 app.use("/admin", adminRoutes);
 
 app.get("/health", (_, res) => res.json({ ok: true, ts: Date.now() }));
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => {
+        console.error("MongoDB error:", err);
+        process.exit(1);
+    });
 
 export default app;
